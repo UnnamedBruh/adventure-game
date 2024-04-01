9488a77acde023bbf9b03fa9cfc00250f9fa3226
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-03-31 18:13:52 -0700

    Update adventure.js

--- adventure.js
+++ adventure.js
@@ -5,13 +5,16 @@ const adventure = document.querySelector("p")
 function next(option1, json = {}) {
   if (option1 == 0) {
     const json2 = {
-      name: name.value
+      name: name.value,
+			br: document.createElement("br")
     }
 
     name.remove()
 
     adventure.innerHTML = ""
-    adventure.innerText = "\"" + name + "?\", the orb curiously says your name out loud. \"That is a very interesting name.\""
+  	adventure.innerText = "\"" + json2.name + "?\", the orb curiously says your name out loud. \"That is a very interesting name\", the orb says. The orb leads you to a portal that is also white."
+		adventure.appendChild(json2.br)
+		adventure.innerText += "Would you go in the portal?"
     img.src = "start1.png"
   }
 }
